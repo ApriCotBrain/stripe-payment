d4f2daf0fdc24362cdee067f76c6b99ff18a9d25
Ольга Мелихова <melihova.o@yandex.ru>
2024-02-26 17:30:03 +0300

    feat: start django-project

---
+++ stripe_payment/stripe_payment/wsgi.py
@@ -0,0 +1,9 @@
+"""URL configuration of the 'stripe_payment' application."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stripe_payment.settings")
+
+application = get_wsgi_application()
